1f45a4847b6a63d58562a061eb95840255ac8914
Pedram Rahmanian <pedram@gutenmade.com>
2020-07-28 09:34:35 -0500

    modified CORS

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
-  CORS(app)
+  CORS(app, resources={r"/*": {"origins":"*"}})
 
   @app.route('/')
   def get_greeting():
