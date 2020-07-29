6531afe1f816a7fa7fec473d01bf794e030fda11
Pedram Rahmanian <pedram@gutenmade.com>
2020-07-29 17:22:43 -0500

    fixed manage app reference

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ from flask_migrate import Migrate, MigrateCommand
 from app import mooves_app
 from models import db
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+migrate = Migrate(mooves_app, db)
+manager = Manager(mooves_app)
 
 manager.add_command('db', MigrateCommand)
 
