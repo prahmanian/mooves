d14bab3da8a1bea417afa0d5d2cb7c0e849ab0da
Pedram Rahmanian <pedram@gutenmade.com>
2020-07-28 22:38:21 -0500

    adjust env variables

--- app.py
+++ app.py
@@ -30,12 +30,19 @@ def create_app(test_config=None):
   if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-  AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
-  AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
-  AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
-  AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+  # AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
+  # AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
+  # AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
+  # AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
+  # AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+  # AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+
+  AUTH0_CALLBACK_URL = env.get(AUTH0_CALLBACK_URL)
+  AUTH0_CLIENT_ID = env.get(AUTH0_CLIENT_ID)
+  AUTH0_CLIENT_SECRET = env.get(AUTH0_CLIENT_SECRET)
+  AUTH0_DOMAIN = env.get(AUTH0_DOMAIN)
   AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
-  AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
+  AUTH0_AUDIENCE = env.get(AUTH0_AUDIENCE)
   
   oauth = OAuth(app)
 
